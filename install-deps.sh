3f467f5760c5c791924dcb683abf0b9f912bb15f
Larry Botha <larry@fixate.it>
2020-12-30 17:56:27 +0200

    clean up spacing and comments, remove unneeded things

--- install-deps.sh
+++ install-deps.sh
@@ -1,14 +1,10 @@
 # up to you (me) if you want to run this as a file or copy paste at your leisure
 
 
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-
 # homebrew!
 # you need the code CLI tools YOU FOOL.
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
 
 # install Node Version Manager and node
 if type -P nvm &> /dev/null; then
@@ -18,10 +14,11 @@ else
 
 	curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
 
-  echo "installing node"
+	echo "installing node"
 
-  nvm install node
+	nvm install node
 fi
+
 
 # make a code directory for dependencies
 if [ ! -d ~/code ]; then
@@ -36,14 +33,13 @@ if [ ! -d ~/code/antigen ]; then
 fi
 
 
+# z
 # https://github.com/rupa/z
-# z, oh how i love you
+# z binary is already referenced from .shrc
 cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
-
-# z binary is already referenced from .bash_profile
 
 
 # https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
@@ -55,26 +51,8 @@ cd ~/code/git-completion
 curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -OL
 chmod -X ~/code/git-completion/git-completion.bash
 
-# git-completion binary is already referenced from .bash_profile
-
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
-
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
+# git-completion binary is already referenced from .shrc
 
 
 # for the c alias (syntax highlighted cat)
 sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
