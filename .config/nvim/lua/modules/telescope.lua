d8773c42f0ecb374d418136b1900033f65b9c6c8
Larry Botha <larry@fixate.it>
2021-07-04 11:11:11 +0200

    add fuzzy option to telescope fzf native

--- .config/nvim/lua/modules/telescope.lua
+++ .config/nvim/lua/modules/telescope.lua
@@ -38,6 +38,7 @@ require("telescope").setup {
     },
     extensions = {
         fzf = {
+            fuzzy = true,
             override_generic_sorter = true, -- override the generic sorter
             override_file_sorter = true, -- override the file sorter
             case_mode = "smart_case" -- or "ignore_case" or "respect_case"
