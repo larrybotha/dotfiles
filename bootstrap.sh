7966f29badfb4a4e992dff252a81129ac11831a5
Larry Botha <larry@fixate.it>
2021-07-30 13:45:45 +0200

    reorganise dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,20 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
-function doIt() {
+function heading() {
+  local divider="==================================="
+  echo -e "\n${divider}"
+  echo -e "  $*"
+  echo ${divider}
+}
+
+function log {
+  echo -e "$*"
+}
+
+function sync_files() {
+  heading "syncing dotfiles"
+
   rsync --exclude ".git/" \
     --exclude ".DS_Store" \
     --exclude "bootstrap.sh" \
@@ -14,11 +27,21 @@ function doIt() {
     --exclude ".config" \
     --exclude ".tmux.conf" \
     --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+}
+
+function prepare_completions() {
+  heading "preparing completions"
 
   # append npm script completion
   if type "npm" >/dev/null; then
     npm completion >>~/.sh_completion
   fi
+
+  log 'done'
+}
+
+function source_shell() {
+  heading "sourcing ${SHELL}"
 
   if [ "$SHELL" == "/bin/zsh" ]; then
     zsh ~/.zshrc
@@ -26,13 +49,13 @@ function doIt() {
     source ~/.bash_profile
   fi
 
-  symlinkConfigs
-  copyFiles
+  log 'done'
 }
 
-function symlinkConfigs() {
-  echo ""
-  configs=(
+function symlink_configs() {
+  heading "symlinking configs"
+
+  local configs=(
     ".vimrc"
     ".vim"
     ".tmux.conf"
@@ -50,28 +73,28 @@ function symlinkConfigs() {
   )
 
   for config in "${configs[@]}"; do
-    user_path=$HOME/$config
-    config_path=$PWD/$config
+    local user_path=$HOME/$config
+    local config_path=$PWD/$config
 
     if [ ! -e "$user_path" ]; then
-      echo -e "linking: ~/$config -> $config"
-      folder=${user_path%/*}
+      log "linking: ~/$config -> $config"
+      local folder=${user_path%/*}
       mkdir -p $folder
       ln -s $config_path $user_path
     elif [ -L "$user_path" ]; then
-      echo -e "linked: ~/$config -> ./$config"
+      log "linked: ~/$config -> ./$config"
     else
-      echo -e "not linked: ~/$config -> ./$config"
-      echo -e "\t=> consider removing ~/$config"
+      log "not linked: ~/$config -> ./$config\n\t=> consider removing ~/$config"
     fi
   done
 }
 
 # TODO: use symlinks for this to prevent having to source bootstrap.sh when
 # changes are made
-function copyFiles() {
-  echo ""
-  configs=(
+function copy_files() {
+  heading "copying files"
+
+  local configs=(
     "
       vimspector-global-config.json
       $PWD/.vim/local/plugins/vimspector
@@ -81,20 +104,20 @@ function copyFiles() {
 
   for config in "${configs[@]}"; do
     set -- $config
-    file_name=$1
-    source_file=$2/$file_name
-    dest_file=$3/$file_name
+    local file_name=$1
+    local source_file=$2/$file_name
+    local dest_file=$3/$file_name
 
     if [ ! -d "$2" ]; then
-      echo -e "creating folder: $dest_path"
+      log "creating folder: $dest_path"
       mkdir -p $dest_path
     fi
 
     if [ ! -e "$dest_file" ]; then
-      echo -e "copying: $source_file \n\t-> $dest_file"
+      log "copying: $source_file \n\t-> $dest_file"
       cp $source_file $dest_file
     else
-      echo -e "replacing: $source_file \n\t-> $dest_file"
+      log "replacing: $source_file \n\t-> $dest_file"
       cp -f $source_file $dest_file
     fi
   done
@@ -125,14 +148,27 @@ function update_nnn_plugins() {
   symlink_custom_nnn_plugins
 }
 
+function do_it() {
+  sync_files
+  prepare_completions
+  symlink_configs
+  copy_files
+  update_nnn_plugins
+  source_shell
+}
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
+  do_it
 else
   read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
+    do_it
   fi
 fi
 
-unset doIt copyFiles symlinkConfigs
+unset do_it \
+  copy_files \
+  symlink_configs \
+  update_nnn_plugins \
+  log heading
