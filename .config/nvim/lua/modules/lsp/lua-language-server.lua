f69b04373f6a76a1797d9bca69109e5faf729bc3
Larry Botha <lawrenceguy@gmail.com>
2022-04-04 13:01:20 +0200

    fix(lua-language-server.lua): update lua binary path

--- .config/nvim/lua/modules/lsp/lua-language-server.lua
+++ .config/nvim/lua/modules/lsp/lua-language-server.lua
@@ -2,22 +2,10 @@
 --install the lua-language-server
 
 local M = {}
-local system_name
-
-if vim.fn.has("mac") == 1 then
-    system_name = "macOS"
-elseif vim.fn.has("unix") == 1 then
-    system_name = "Linux"
-elseif vim.fn.has("win32") == 1 then
-    system_name = "Windows"
-else
-    print("Unsupported system for sumneko")
-end
-
 -- install the language server at ~/code/lua-language-server
 -- see https://github.com/sumneko/lua-language-server/wiki/Build-and-Run
 local sumneko_root_path = os.getenv("HOME") .. "/code/lua-language-server"
-local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
 
 function M.setup(opts)
     opts = opts or {}
