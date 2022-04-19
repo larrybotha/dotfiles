9bb89b74b16781794778fea4288c14a3effb113f
Larry Botha <lawrenceguy@gmail.com>
2022-04-19 12:57:59 +0200

    chore(treesitter.lua): update installed packages as per prompt

--- .config/nvim/lua/modules/treesitter.lua
+++ .config/nvim/lua/modules/treesitter.lua
@@ -1,6 +1,6 @@
 require "nvim-treesitter.configs".setup {
-    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    ensure_installed = "maintained",
+    -- one of "all", or a list of languages
+    ensure_installed = "all",
     -- List of parsers to ignore installing
     --ignore_install = { "javascript" },
     highlight = {
