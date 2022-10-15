76720d21f01fd23ca0ebf9872e31e6efea0e8933
Larry Botha <lawrenceguy@gmail.com>
2022-10-15 15:38:42 -0500

    fix(lsp/init.lua): update deprecated function

--- .config/nvim/lua/modules/lsp/init.lua
+++ .config/nvim/lua/modules/lsp/init.lua
@@ -91,7 +91,7 @@ local server_configs = {
 for _, config in ipairs(server_configs) do
     local attach_options = {
         on_attach = on_attach,
-        capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+        capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
     }
     local options = config.options
 
