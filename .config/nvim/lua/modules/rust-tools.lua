0454fc59d3a4e9f21264c0a8c5df72b6e5b4ef9d
Larry Botha <lawrenceguy@gmail.com>
2022-12-27 10:47:33 +0200

    chore(karabiner.json): update settings

--- .config/nvim/lua/modules/rust-tools.lua
+++ .config/nvim/lua/modules/rust-tools.lua
@@ -3,12 +3,36 @@ local nvim_lsp = require "./modules/lsp/init"
 local options = {
     tools = {
         autoSetHints = true,
-        hover_with_actions = true,
+        hover_actions = {
+            -- the border that is used for the hover window
+            -- see vim.api.nvim_open_win()
+            border = {
+                {"╭", "FloatBorder"},
+                {"─", "FloatBorder"},
+                {"╮", "FloatBorder"},
+                {"│", "FloatBorder"},
+                {"╯", "FloatBorder"},
+                {"─", "FloatBorder"},
+                {"╰", "FloatBorder"},
+                {"│", "FloatBorder"}
+            },
+            max_width = nil,
+            max_height = nil,
+            auto_focus = false
+        },
         runnables = {use_telescope = true},
         inlay_hints = {
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = ""
+            -- Only show inlay hints for the current line
+            --only_current_line = false,
+            -- whether to show parameter hints with the inlay hints or not
+            -- default: true
+            show_parameter_hints = true,
+            -- prefix for parameter hints
+            -- default: "<-"
+            parameter_hints_prefix = "<- ",
+            -- prefix for all the other hints (type, chaining)
+            -- default: "=>"
+            other_hints_prefix = "=> "
         }
     },
     -- all the options to send to nvim-lspconfig
@@ -20,6 +44,8 @@ local options = {
             -- to enable rust-analyzer settings visit:
             -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
             ["rust-analyzer"] = {
+                -- HACK: https://github.com/simrat39/rust-tools.nvim/issues/300
+                inlayHints = {locationLinks = false},
                 -- enable clippy on save
                 checkOnSave = {
                     command = "clippy"
