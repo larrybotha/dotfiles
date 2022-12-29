c6d62211f4be27a84e7ba0014788c209ae81d3af
Larry Botha <lawrenceguy@gmail.com>
2022-12-29 13:59:01 +0200

    feat(install-deps.sh): install evcxr Rust repl

--- install-deps.sh
+++ install-deps.sh
@@ -6,30 +6,43 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 # install Node Version Manager and node
 if type -p nvm &>/dev/null; then
-  echo "nvm already installed"
+	echo "nvm already installed"
 else
-  echo "installing nvm"
+	echo "installing nvm"
 
-  curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
+	curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | bash
 fi
 
 if type -p node &>/dev/null; then
-  echo "node already installed"
+	echo "node already installed"
 else
-  echo "installing node"
+	echo "installing node"
 
-  nvm install node
+	nvm install node
+fi
+
+if type -p evcxr &>/dev/null; then
+	echo "evcxr already installed"
+elif
+	! type -p cargo &
+	/dev/null
+then
+	echo "unable to install evcxr - unable to find Rust's 'cargo' binary"
+else
+	echo "installing evcxr"
+
+	cargo install evcxr
 fi
 
 # make a code directory for dependencies
 if [ ! -d ~/code ]; then
-  mkdir ~/code
+	mkdir ~/code
 fi
 
 # https://github.com/git/git/blob/master/contrib/completion/git-completion.bash
 # Autocomplete motherflippin' git branches
 if [ ! -d ~/code/git-completion ]; then
-  mkdir ~/code/git-completion
+	mkdir ~/code/git-completion
 fi
 pushd ~/code/git-completion/ >/dev/null
 curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -OL
