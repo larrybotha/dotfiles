c23bfa00464c5989dbc22dac58d270fce3afe858
Larry Botha <lawrenceguy@gmail.com>
2023-05-29 17:11:24 -0500

    style(trouble.lua): dedupe options

--- .config/nvim/lua/modules/trouble.lua
+++ .config/nvim/lua/modules/trouble.lua
@@ -2,9 +2,13 @@ local trouble = require("trouble")
 
 trouble.setup()
 
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+local options = {
+	silent = true,
+	noremap = true,
+}
+
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", options)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", options)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", options)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", options)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", options)
