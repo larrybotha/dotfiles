e7699f3bc102d3bb2836256f09e0d189611b0f12
Larry Botha <lawrenceguy@gmail.com>
2023-05-29 16:37:08 -0500

    fix(formatter.nvim): allow for configuring parser for prettierd

--- .config/nvim/lua/modules/formatter/custom.lua
+++ .config/nvim/lua/modules/formatter/custom.lua
@@ -124,6 +124,22 @@ local M = {
 		}
 	end,
 
+	prettierd = function(parser)
+		local args = { util.escape_path(util.get_current_buffer_file_path()) }
+
+		if parser then
+			table.insert(args, "--parser=" .. parser)
+		end
+
+		return function()
+			return {
+				exe = "prettierd",
+				args = args,
+				stdin = true,
+			}
+		end
+	end,
+
 	shfmt = function()
 		return {
 			exe = "shfmt",
--- .config/nvim/lua/modules/formatter/init.lua
+++ .config/nvim/lua/modules/formatter/init.lua
@@ -10,21 +10,22 @@ formatter.setup({
 
 	filetype = {
 		go = { custom_formatters.goimports, custom_formatters.gofmt },
-		graphql = { formatters.prettierd },
+		graphql = { custom_formatters.prettierd("graphql") },
 		hcl = { custom_formatters.terraform },
-		html = { formatters.prettierd },
+		html = { custom_formatters.prettierd("html") },
 		htmldjango = { custom_formatters.djlint },
-		javascript = { formatters.prettierd },
-		javascriptreact = { formatters.prettierd },
+		javascript = { custom_formatters.prettierd(), formatters.eslint_d },
+		javascriptreact = { custom_formatters.prettierd(), formatters.eslint_d },
 		json = {
-			formatters.prettierd,
+			custom_formatters.prettierd("json"),
 			custom_formatters.fixjson,
 		},
 		lua = { filetypes.lua.stylua },
 		markdown = {
-			formatters.prettierd,
 			custom_formatters.cbfmt,
-			custom_formatters.markdown_toc,
+			-- TODO: determine how to get markdown_toc to update the file without confirmation
+			--custom_formatters.markdown_toc,
+			custom_formatters.prettierd(),
 		},
 		org = { custom_formatters.cbfmt },
 		packer = { custom_formatters.packer },
@@ -39,20 +40,21 @@ formatter.setup({
 			custom_formatters.shellharden,
 		},
 		sql = { custom_formatters.sqlfluff },
-		svelte = { formatters.prettierd },
-		svg = { formatters.prettierd },
+		svelte = { custom_formatters.prettierd("svelte"), formatters.eslint_d },
+		svg = { custom_formatters.prettierd("html") },
 		terraform = { custom_formatters.terraform },
-		typescript = { formatters.prettierd },
-		typescriptreact = { formatters.prettierd },
-		vue = { formatters.prettierd },
+		typescript = { custom_formatters.prettierd("typescript"), formatters.eslint_d },
+		typescriptreact = { custom_formatters.prettierd(), formatters.eslint_d },
+		vue = { custom_formatters.prettierd(), formatters.eslint_d },
 		toml = { custom_formatters.taplo },
-		yaml = { formatters.prettierd },
+		yaml = { custom_formatters.prettierd("yaml") },
 	},
 
-	["svx"] = { formatters.prettierd },
+	["svx"] = { custom_formatters.prettierd() },
 })
 
 local au_group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+
 -- format on save
 vim.api.nvim_create_autocmd("BufWritePost", {
 	pattern = "*",
