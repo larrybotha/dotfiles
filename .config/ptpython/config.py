5724edc4b75afabbd7a90c48161737cd25875b32
Larry Botha <lawrenceguy@gmail.com>
2023-06-15 14:13:58 -0500

    chore(ptpython/config.py): use nord theme

--- .config/ptpython/config.py
+++ .config/ptpython/config.py
@@ -100,8 +100,7 @@ def configure(repl):
     repl.enable_input_validation = True
 
     # Use this colorscheme for the code.
-    repl.use_code_colorscheme("monokai")
-    # repl.use_code_colorscheme("pastie")
+    repl.use_code_colorscheme("nord")
 
     # Set color depth (keep in mind that not all terminals support true color).
 
