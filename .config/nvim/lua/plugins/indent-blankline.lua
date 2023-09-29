c01afd19d6711f068d895c2a5662fc54fa5c1e09
Larry Botha <lawrenceguy@gmail.com>
2023-09-29 09:39:01 -0500

    fix(indent-blankline.lua): use v3 config

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,4 +1,14 @@
-require("indent_blankline").setup({
-	char = "¦", --  ¦ or ┆ or │ or ⎸ or ▏
+local highlight = {
+	"CursorColumn",
+	"Whitespace",
+}
+
+require("ibl").setup({
 	buftype_exclude = { "terminal" },
+	indent = { highlight = highlight, char = "" },
+	whitespace = {
+		highlight = highlight,
+		remove_blankline_trail = false,
+	},
+	scope = { enabled = false },
 })
