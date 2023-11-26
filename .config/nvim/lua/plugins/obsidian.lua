281f4125761579a0dd2a6648306bdf590ff860fe
Larry Botha <lawrenceguy@gmail.com>
2023-11-26 16:20:03 +0200

    refactor(obsidian.lua): use 'workspaces' instead of 'dir' to define vaults

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -1,61 +1,46 @@
 local obsidian = require("obsidian")
 
-local base_options = {
+local function get_workspaces(vaults_dir)
+	local child_dirs = {}
+	local workspaces = {}
+
+	for dir in vim.fs.dir(vaults_dir) do
+		local path = vim.fn.expand(string.format("%s/%s", vaults_dir, dir))
+		table.insert(child_dirs, path)
+	end
+
+	for _, dir in pairs(child_dirs) do
+		local path_parts = {}
+
+		for x in string.gmatch(dir, "[^/]+") do
+			table.insert(path_parts, x)
+		end
+
+		table.insert(workspaces, {
+			name = path_parts[#path_parts],
+			path = dir,
+		})
+	end
+
+	return workspaces
+end
+
+local vaults_dir = "~/projects/obsidian-vaults"
+local options = {
 	-- don't manage frontmatter
 	disable_frontmatter = true,
 
 	mappings = {
 		-- navigate to markdown files using gf - overrides Vim's default behaviour
-		["gf"] = require("obsidian").util.gf_passthrough(),
+		["gf"] = {
+			action = function()
+				return require("obsidian").util.gf_passthrough()
+			end,
+			opts = { noremap = false, expr = true, buffer = true },
+		},
 	},
+
+	workspaces = get_workspaces(vaults_dir),
 }
-local vault_dirs = {}
-local vaults_dir = "~/projects/obsidian-vaults"
 
-for dir in vim.fs.dir(vaults_dir) do
-	local path = vim.fn.expand(string.format("%s/%s", vaults_dir, dir))
-	table.insert(vault_dirs, path)
-end
-
-local function configure_obsidian(vaults, options)
-	local cwd = vim.fn.getcwd()
-	local dirs = { cwd }
-	local vault_dir = nil
-	local vault_map = {}
-
-	for _, dir in pairs(vaults) do
-		vault_map[dir] = true
-	end
-
-	for x in vim.fs.parents(cwd) do
-		table.insert(dirs, x)
-	end
-
-	for _, x in pairs(dirs) do
-		local is_match = vault_map[x]
-
-		if is_match then
-			vault_dir = x
-			break
-		end
-	end
-
-	if vault_dir then
-		local workspace_options = vim.tbl_extend("force", options, { dir = vault_dir })
-
-		obsidian.setup(workspace_options)
-	end
-end
-
-local au_group = vim.api.nvim_create_augroup("ObsidianAutogroup", { clear = true })
-
-vim.api.nvim_create_autocmd("DirChanged", {
-	pattern = "*",
-	group = au_group,
-	callback = function()
-		configure_obsidian(vault_dirs, base_options)
-	end,
-	desc = "Re-initialises Obsidian when the working directory changes",
-})
-
-configure_obsidian(vault_dirs, base_options)
+obsidian.setup(options)
