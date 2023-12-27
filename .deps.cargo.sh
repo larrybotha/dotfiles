dd0d1b4aba534285348e75cf80e4bcc26f182eb5
Larry Botha <lawrenceguy@gmail.com>
2023-12-27 13:46:30 +0200

    chore(.deps.cargo.sh): add cargo-expand

--- .deps.cargo.sh
+++ .deps.cargo.sh
@@ -15,6 +15,8 @@ ensure_has_rust
 cargo install evcxr # repl
 cargo install git-stack # tool for visualising and working with stacked Git branches
 
+cargo install cargo-expand # prints out the expansions of macros
+
 # diagnostics, linting, lsp, formatters
 cargo install blackd-client  # formatter
 cargo install cbfmt          # formatter
