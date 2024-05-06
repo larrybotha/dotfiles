e3f0ed390e4a9e80c4e0be24b97e67f70c9df626
Larry Botha <lawrenceguy@gmail.com>
2024-05-06 10:05:42 +0200

    feat(Lazy): add vim-go and vimtest as TODOs

---
+++ .config/nvim/lua/larrybotha/lazy/vim-go.lua
@@ -0,0 +1,8 @@
+return { {} }
+-- TODO: determine if I'm going to need vim-go, or if there's a better way to
+-- work with go
+
+--" format and rewrite imports on save
+--let g:go_fmt_command = 'goimports'
+--" show type info under cursor
+--let g:go_auto_type_info = 1
