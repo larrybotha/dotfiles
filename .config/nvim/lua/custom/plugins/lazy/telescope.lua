6e8d1475854deb367162db06ce1cad8141ea9ef1
Larry Botha <lawrenceguy@gmail.com>
2024-05-29 10:28:29 +0200

    style(telescope.lua): remove redundant module

--- .config/nvim/lua/custom/plugins/lazy/telescope.lua
+++ .config/nvim/lua/custom/plugins/lazy/telescope.lua
@@ -8,9 +8,10 @@ return {
 			"nvim-telescope/telescope-fzf-native.nvim",
 			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
 		},
+		"nvim-telescope/telescope-ui-select.nvim",
 	},
 	opts = {},
 	config = function()
-		require("custom.plugins.telescope").setup()
+		require("custom.plugins.telescope")
 	end,
 }
--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -1,10 +1,6 @@
-local M = {}
-local custom_actions = {}
-
-function custom_actions.fzf_multi_select(prompt_bufnr)
+local function select_to_quickfix(prompt_bufnr)
 	local actions = require("telescope.actions")
 	local action_state = require("telescope.actions.state")
-
 	local picker = action_state.get_current_picker(prompt_bufnr)
 	local num_selections = #picker:get_multi_selection()
 
@@ -19,71 +15,72 @@ function custom_actions.fzf_multi_select(prompt_bufnr)
 	end
 end
 
-M.setup = function()
-	local telescope = require("telescope")
-	local builtin = require("telescope.builtin")
-	local setKeymap = vim.keymap.set
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
+local setKeymap = vim.keymap.set
 
-	telescope.setup({
-		defaults = {
+telescope.setup({
+	defaults = {
+
+		mappings = {
+			i = { ["<cr>"] = select_to_quickfix },
+			n = { ["<cr>"] = select_to_quickfix },
+		},
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--hidden",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+		},
+	},
+	["ui-select"] = {
+		require("telescope.themes").get_dropdown({}),
+	},
+	extensions = {
+		fzf = {
+			fuzzy = true,
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+		},
+	},
+	pickers = {
+		live_grep = {
 			mappings = {
-				i = { ["<cr>"] = custom_actions.fzf_multi_select },
-				n = { ["<cr>"] = custom_actions.fzf_multi_select },
-			},
-			vimgrep_arguments = {
-				"rg",
-				"--color=never",
-				"--hidden",
-				"--no-heading",
-				"--with-filename",
-				"--line-number",
-				"--column",
-				"--smart-case",
-			},
-		},
-		extensions = {
-			fzf = {
-				fuzzy = true,
-				override_generic_sorter = true, -- override the generic sorter
-				override_file_sorter = true, -- override the file sorter
-				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			},
-		},
-		pickers = {
-			live_grep = {
-				mappings = {
-					n = {
-						-- switch to filtering by filename during live_grep
-						["<c-f>"] = require("telescope.actions").to_fuzzy_refine,
-					},
+				n = {
+					-- switch to filtering by filename during live_grep
+					["<c-f>"] = require("telescope.actions").to_fuzzy_refine,
 				},
 			},
 		},
-	})
+	},
+})
 
-	require("telescope").load_extension("fzf")
-	require("telescope").load_extension("asynctasks")
+pcall(require("telescope").load_extension("fzf"))
+pcall(require("telescope").load_extension("asynctasks"))
+pcall(require("telescope").load_extension, "ui-select")
 
-	setKeymap("n", "<C-p>", function()
-		builtin.find_files({ hidden = true })
-	end)
-	setKeymap("n", "<leader>fg", builtin.live_grep, {})
-	setKeymap("n", "<leader>fb", builtin.buffers, {})
-	setKeymap("n", "<leader>fh", builtin.help_tags, {})
-	setKeymap("n", "<leader>fa", builtin.builtin, {})
-	setKeymap("n", "<leader>fr", function()
-		builtin.lsp_references({ trim_text = true })
-	end)
-	setKeymap("n", "<leader>fR", function()
-		vim.lsp.buf.references()
-	end, { buffer = true }) -- add references to quickfix
-	setKeymap("n", "<leader>ft", telescope.extensions.asynctasks.all, {})
+setKeymap("n", "<C-p>", function()
+	builtin.find_files({ hidden = true })
+end)
+setKeymap("n", "<leader>fg", builtin.live_grep, {})
+setKeymap("n", "<leader>fb", builtin.buffers, {})
+setKeymap("n", "<leader>fh", builtin.help_tags, {})
+setKeymap("n", "<leader>fa", builtin.builtin, {})
+setKeymap("n", "<leader>fr", function()
+	builtin.lsp_references({ trim_text = true })
+end)
+setKeymap("n", "<leader>fR", function()
+	vim.lsp.buf.references()
+end, { buffer = true }) -- add references to quickfix
+setKeymap("n", "<leader>ft", telescope.extensions.asynctasks.all, {})
 
-	-- Diagnostics
-	setKeymap("n", "<leader>fd", builtin.diagnostics, {}) -- all files
-	setKeymap("n", "<leader>fD", function()
-		builtin.diagnostics({ bufnr = 0 }) -- current file
-	end)
-end
-
-return M
+-- Diagnostics
+setKeymap("n", "<leader>fd", builtin.diagnostics, {}) -- all files
+setKeymap("n", "<leader>fD", function()
+	builtin.diagnostics({ bufnr = 0 }) -- current file
+end)
