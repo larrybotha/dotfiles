628e87520ee08b4fa3cf36556718f58553ef0083
Larry Botha <lawrenceguy@gmail.com>
2024-05-31 11:03:06 +0200

    fix(trouble.lua): update config to v3

--- .config/nvim/lua/custom/plugins/lazy/trouble.lua
+++ .config/nvim/lua/custom/plugins/lazy/trouble.lua
@@ -1,18 +1,37 @@
 return {
 	"folke/trouble.nvim",
 
-	config = function()
-		require("trouble").setup()
-
-		local opts = {
-			silent = true,
-			noremap = true,
-		}
-
-		vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
-		vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", opts)
-		vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", opts)
-		vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", opts)
-		vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", opts)
-	end,
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xcs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>xcl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
+	opts = {},
 }
