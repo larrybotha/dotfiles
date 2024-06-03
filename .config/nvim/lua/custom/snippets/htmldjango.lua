e19a276d57e92cb22273fb0b5117d1380df3ddbd
Larry Botha <lawrenceguy@gmail.com>
2024-06-03 17:05:48 +0200

    feat(htmldjango.lua): add 'component' snippet

--- .config/nvim/lua/custom/snippets/htmldjango.lua
+++ .config/nvim/lua/custom/snippets/htmldjango.lua
@@ -7,10 +7,11 @@ local i = luasnip.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 luasnip.add_snippets("htmldjango", {
+	s("comment", fmt("{{% comment %}}\n{}\n{{% endcomment %}}", { i(0) })),
+	s("component", fmt("{{% component {} %}}\n{}\n{{% endcomponent %}}", { i(1), i(0) })),
 	s("djlint", fmt("{{# djlint:off #}}\n{}\n{{# djlint:on #}}", { i(0) })),
 	s("elif", fmt("{{% elif {} %}}\n{}\n", { i(1), i(0) })),
 	s("for", fmt("{{% for {} in {} %}}\n{}\n{{% empty %}}\n{}\n{{% endfor %}}", { i(1), i(2), i(3), i(0) })),
 	s("if", fmt("{{% if {} %}}\n{}\n{{% endif %}}", { i(1), i(0) })),
 	s("spaceless", fmt("{{% spaceless %}}\n{}\n{{% endspaceless %}}", { i(0) })),
-	s("comment", fmt("{{% comment %}}\n{}\n{{% endcomment %}}", { i(0) })),
 })
