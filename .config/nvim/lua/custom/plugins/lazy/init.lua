79fffbf9125c1cd043679cbf7be8f8e16454db81
Larry Botha <lawrenceguy@gmail.com>
2024-06-03 17:06:11 +0200

    feat(nvim): replace vim-surround with nvim-surround

--- .config/nvim/lua/custom/plugins/lazy/init.lua
+++ .config/nvim/lua/custom/plugins/lazy/init.lua
@@ -2,7 +2,7 @@ return {
 	{ "nvim-lua/plenary.nvim" },
 	{ "tpope/vim-sensible" },
 	{ "bullets-vim/bullets.vim" },
-	{ "tpope/vim-surround" },
+	{ "kylechui/nvim-surround", config = true },
 	{ "wakatime/vim-wakatime" },
 	{ "mg979/vim-visual-multi" },
 	{ "rhysd/git-messenger.vim" },
