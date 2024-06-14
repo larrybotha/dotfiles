ab2c78a8ef877cae0a14b678671ab96ff7c76ee2
Larry Botha <lawrenceguy@gmail.com>
2024-06-14 13:25:05 +0200

    feat(htmldjango.lua): add snippets

--- .config/nvim/lua/custom/snippets/htmldjango.lua
+++ .config/nvim/lua/custom/snippets/htmldjango.lua
@@ -3,7 +3,6 @@ require("luasnip.session.snippet_collection").clear_snippets("htmldjango")
 local luasnip = require("luasnip")
 local s = luasnip.snippet
 local i = luasnip.insert_node
-
 local fmt = require("luasnip.extras.fmt").fmt
 
 luasnip.add_snippets("htmldjango", {
@@ -12,6 +11,9 @@ luasnip.add_snippets("htmldjango", {
 	s("djlint", fmt("{{# djlint:off #}}\n{}\n{{# djlint:on #}}", { i(0) })),
 	s("elif", fmt("{{% elif {} %}}\n{}\n", { i(1), i(0) })),
 	s("for", fmt("{{% for {} in {} %}}\n{}\n{{% empty %}}\n{}\n{{% endfor %}}", { i(1), i(2), i(3), i(0) })),
+	s("if", fmt("{{% if {} %}}\n{}\n{{% else %}}\n{}\n{{% endif %}}", { i(2), i(1), i(0) })),
 	s("if", fmt("{{% if {} %}}\n{}\n{{% endif %}}", { i(1), i(0) })),
+	s("include", fmt('{{% include "{}"{} %}}', { i(1), i(0) })),
+	s("load", fmt("{{% load {} %}}", { i(0) })),
 	s("spaceless", fmt("{{% spaceless %}}\n{}\n{{% endspaceless %}}", { i(0) })),
 })
