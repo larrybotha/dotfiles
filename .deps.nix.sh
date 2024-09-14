4430328038c45a27b5807e4ec6e9757a3f1ad3a1
Larry Botha <lawrenceguy@gmail.com>
2024-09-14 11:56:13 +0530

    feat(.deps.nix.sh): install just

--- .deps.nix.sh
+++ .deps.nix.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 function ensure_has_nix() {
-	if type -p nix &>/dev/null; then
-		echo "Nix is already installed"
-	else
+	if ! type -p nix &>/dev/null; then
 		echo "installing nix"
 
     curl -L https://nixos.org/nix/install | sh -s -- --daemon
@@ -15,6 +13,8 @@ ensure_has_nix
 packages=(
   alejandra # formatter
   deadnix # diagnostics
+
+  just # task runner
 )
 
 nix-env -i "${packages[@]}"
