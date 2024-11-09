1a3ecb22478de1dc4ae0056721b9c6adf1bb8436
Larry Botha <lawrenceguy@gmail.com>
2024-11-09 14:51:43 +0530

    feat(snippts): add go snippets

---
+++ .config/nvim/lua/custom/snippets/go.lua
@@ -0,0 +1,12 @@
+require("luasnip.session.snippet_collection").clear_snippets("svelte")
+
+local luasnip = require("luasnip")
+local s = luasnip.snippet
+local i = luasnip.insert_node
+
+local fmt = require("luasnip.extras.fmt").fmt
+
+luasnip.add_snippets("go", {
+	s("iferr", fmt("if err != nil {{\n\treturn {}\n}}", { i(0) })),
+	s("frange", fmt("for {2}, {1} := range {{\n\t{3}\n}}", { i(2, "v"), i(1, "i"), i(3) })),
+})
