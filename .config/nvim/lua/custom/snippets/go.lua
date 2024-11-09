92c80c6443441c797f42f9efc0d686e8fdad5463
Larry Botha <lawrenceguy@gmail.com>
2024-11-09 20:08:04 +0530

    fix(go snippets): add placeholder for range argument

--- .config/nvim/lua/custom/snippets/go.lua
+++ .config/nvim/lua/custom/snippets/go.lua
@@ -8,5 +8,5 @@ local fmt = require("luasnip.extras.fmt").fmt
 
 luasnip.add_snippets("go", {
 	s("iferr", fmt("if err != nil {{\n\treturn {}\n}}", { i(0) })),
-	s("frange", fmt("for {2}, {1} := range {{\n\t{3}\n}}", { i(2, "v"), i(1, "i"), i(3) })),
+	s("frange", fmt("for {2}, {1} := range {3} {{\n\t{4}\n}}", { i(2, "v"), i(1, "i"), i(3), i(4) })),
 })
