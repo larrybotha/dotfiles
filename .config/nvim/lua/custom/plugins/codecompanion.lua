c801ad0a23df0b1c0813079b14b04d97e0b2d32f
Larry Botha <lawrenceguy@gmail.com>
2025-04-20 10:50:32 +0200

    refactor(codecompanion.lua): don't shadow builtin c-a keymap

--- .config/nvim/lua/custom/plugins/codecompanion.lua
+++ .config/nvim/lua/custom/plugins/codecompanion.lua
@@ -27,7 +27,7 @@ cc.setup({
 	},
 })
 
-vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+--vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
 vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
