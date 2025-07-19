3106add73914e7a174bd557d6b9f820e4611394a
Larry Botha <lawrenceguy@gmail.com>
2025-07-19 16:59:41 +0200

    refactor(nvim colorscheme): simplify config

--- home/.config/nvim/lua/custom/plugins/lazy/colors.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/colors.lua
@@ -1,46 +1,29 @@
-local function doTheColours()
-	-- Define custom cursor color highlights
-	local cursorline_opts = { bold = true, bg = "black" }
-	vim.api.nvim_set_hl(0, "CursorLine", cursorline_opts)
-	vim.api.nvim_set_hl(0, "CursorColumn", cursorline_opts)
-
-	-- Highlight trailing white space
-	local errorHl = vim.api.nvim_get_hl(0, { name = "Error" })
-	vim.api.nvim_set_hl(0, "ExtraWhiteSpace", { bg = errorHl.fg })
-	vim.cmd([[match ExtraWhitespace /\s\+$/]])
-
-	vim.api.nvim_set_hl(0, "NvimInternalError", { reverse = true })
-end
-
 return {
 	{
 		"mellow-theme/mellow.nvim",
 		name = "mellow",
-		lazy = false,
+		event = "VeryLazy",
 		priority = 1000,
 		init = function()
-			local colors = require("mellow.colors").dark
-
 			vim.cmd.colorscheme("mellow")
 
+			local colors = require("mellow.colors").dark
 			local highlights = {
 				["TelescopeBorder"] = { link = "FloatBorder" },
-				["TelescopeNormal"] = { link = "Normal" },
-				["TelescopeTitle"] = { fg = colors.magenta },
-				["TelescopePreviewTitle"] = { link = "TelescopeTitle" },
-				["TelescopeResultsTitle"] = { link = "TelescopeTitle" },
-				["TelescopePromptTitle"] = { link = "TelescopeTitle" },
-				["TelescopePromptBorder"] = { fg = colors.gray01, bg = colors.gray01 },
-				["TelescopePromptNormal"] = { fg = colors.gray06, bg = colors.gray01 },
-				["TelescopePromptCounter"] = { fg = colors.gray04, bg = colors.gray01 },
 				["TelescopeMatching"] = { link = "Search" },
+				["TelescopeNormal"] = { link = "Normal" },
+				["TelescopePreviewTitle"] = { link = "TelescopeTitle" },
+				["TelescopePromptBorder"] = { fg = colors.gray01, bg = colors.gray01 },
+				["TelescopePromptCounter"] = { fg = colors.gray04, bg = colors.gray01 },
+				["TelescopePromptNormal"] = { fg = colors.gray06, bg = colors.gray01 },
+				["TelescopePromptTitle"] = { link = "TelescopeTitle" },
+				["TelescopeResultsTitle"] = { link = "TelescopeTitle" },
+				["TelescopeTitle"] = { fg = colors.magenta },
 			}
 
-			for k, v in pairs(highlights) do
-				vim.api.nvim_set_hl(0, k, v)
+			for group, opts in pairs(highlights) do
+				vim.api.nvim_set_hl(0, group, opts)
 			end
-
-			doTheColours()
 		end,
 	},
 	{
