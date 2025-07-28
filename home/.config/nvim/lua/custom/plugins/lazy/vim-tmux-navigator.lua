469f06b15b393e731dc5630ecadfe66cad099d63
Larry Botha <lawrenceguy@gmail.com>
2025-07-28 11:29:04 +0200

    refactor(vim-tmux-navigator.lua): use recommended lazy config

--- home/.config/nvim/lua/custom/plugins/lazy/vim-tmux-navigator.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/vim-tmux-navigator.lua
@@ -1,40 +1,20 @@
--- See: https://github.com/christoomey/vim-tmux-navigator/issues/295#issuecomment-1123455337
-local function setIsVim(is_vim)
-	local command
-
-	if is_vim then
-		command = "silent !tmux set-option -p @custom_is_vim yes"
-	else
-		command = "silent !tmux set-option -p -u @custom_is_vim"
-	end
-
-	vim.api.nvim_exec2(command, { output = true })
-end
-
-local CustomVimTmuxNavigatorGroup = vim.api.nvim_create_augroup("CustomVimTmuxNavigatorGroup", {})
-
 return {
 	{
 		"christoomey/vim-tmux-navigator",
-		name = "vim-tmux-navigator",
-		config = function()
-			vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
-				callback = function()
-					setIsVim(true)
-				end,
-				desc = "set an option in tmux indicating that we're in vim",
-				group = CustomVimTmuxNavigatorGroup,
-				pattern = "*",
-			})
-
-			vim.api.nvim_create_autocmd({ "VimLeavePre", "VimSuspend" }, {
-				callback = function()
-					setIsVim(false)
-				end,
-				desc = "set an option in tmux indicating that we're not in vim",
-				group = CustomVimTmuxNavigatorGroup,
-				pattern = "*",
-			})
-		end,
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
 	},
 }
