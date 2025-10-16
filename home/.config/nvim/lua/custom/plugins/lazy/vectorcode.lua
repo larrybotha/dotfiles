a1b337841ba2c84781a19f781498ba7d58e41a0a
Larry Botha <lawrenceguy@gmail.com>
2025-10-16 07:27:28 +0200

    feat: add vectorcode

---
+++ home/.config/nvim/lua/custom/plugins/lazy/vectorcode.lua
@@ -0,0 +1,39 @@
+return {
+	"Davidyz/VectorCode",
+	version = "*",
+	build = "uv tool upgrade vectorcode",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cond = function()
+		return vim.fn.executable("vectorcode") == 1
+	end,
+	config = function()
+		require("vectorcode").setup(
+			---@type VectorCode.Opts
+			{
+				cli_cmds = {
+					vectorcode = "vectorcode",
+				},
+				---@type VectorCode.RegisterOpts
+				async_opts = {
+					debounce = 10,
+					events = { "BufWritePost", "InsertEnter", "BufReadPost" },
+					exclude_this = true,
+					n_query = 1,
+					notify = false,
+					query_cb = require("vectorcode.utils").make_surrounding_lines_cb(-1),
+					run_on_register = false,
+				},
+				async_backend = "default", -- default|lsp
+				exclude_this = true,
+				n_query = 1,
+				notify = true,
+				timeout_ms = 5000,
+				on_setup = {
+					update = true,
+					lsp = false,
+				},
+				sync_log_env_var = false,
+			}
+		)
+	end,
+}
