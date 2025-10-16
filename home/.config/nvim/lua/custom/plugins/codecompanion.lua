da693ef1f4512904686e8a583e9b059d300622cd
Larry Botha <lawrenceguy@gmail.com>
2025-10-16 10:21:45 +0200

    feat(codecompanion): add vectorcode extension

--- home/.config/nvim/lua/custom/plugins/codecompanion.lua
+++ home/.config/nvim/lua/custom/plugins/codecompanion.lua
@@ -1,3 +1,5 @@
+---@module "vectorcode"
+
 local cc = require("codecompanion")
 local spinner = require("custom.plugins.custom-spinner")
 
@@ -47,6 +49,48 @@ cc.setup({
 	},
 	opts = {
 		log_level = "ERROR", -- TRACE|DEBUG|ERROR|INFO
+	},
+	extensions = {
+		vectorcode = {
+			---@type VectorCode.CodeCompanion.ExtensionOpts
+			opts = {
+				tool_group = {
+					-- this will register a tool group called `@vectorcode_toolbox` that contains all 3 tools
+					enabled = true,
+					-- a list of extra tools that you want to include in `@vectorcode_toolbox`.
+					-- if you use @vectorcode_vectorise, it'll be very handy to include
+					-- `file_search` here.
+					extras = {},
+					collapse = false, -- whether the individual tools should be shown in the chat
+				},
+				tool_opts = {
+					---@type VectorCode.CodeCompanion.ToolOpts
+					["*"] = {},
+					---@type VectorCode.CodeCompanion.LsToolOpts
+					ls = {},
+					---@type VectorCode.CodeCompanion.VectoriseToolOpts
+					vectorise = {},
+					---@type VectorCode.CodeCompanion.QueryToolOpts
+					query = {
+						max_num = { chunk = -1, document = -1 },
+						default_num = { chunk = 50, document = 10 },
+						include_stderr = false,
+						use_lsp = false,
+						no_duplicate = true,
+						chunk_mode = false,
+						---@type VectorCode.CodeCompanion.SummariseOpts
+						summarise = {
+							---@type boolean|(fun(chat: CodeCompanion.Chat, results: VectorCode.QueryResult[]):boolean)|nil
+							enabled = false,
+							adapter = nil,
+							query_augmented = true,
+						},
+					},
+					files_ls = {},
+					files_rm = {},
+				},
+			},
+		},
 	},
 })
 
--- home/.config/nvim/lua/custom/plugins/lazy/codecompanion.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/codecompanion.lua
@@ -1,10 +1,12 @@
 return {
 	"olimorris/codecompanion.nvim",
 	dependencies = {
+		"Davidyz/VectorCode",
 		"nvim-lua/plenary.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function()
 		require("custom.plugins.codecompanion")
 	end,
+	event = "VeryLazy",
 }
