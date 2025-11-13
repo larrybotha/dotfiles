d0519f61ab1a12b684fe663eaa49cab744bb048a
Larry Botha <lawrenceguy@gmail.com>
2025-11-13 23:03:44 +0200

    refactor(markdown): use 2 spaces for markdown, consistent with prettier

---
+++ home/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+-- use indentation of 2 spaces - consistent with prettier formatting
+-- See Neovim's ftplugin/markdown.vim for defaults
+vim.g.markdown_recommended_style = 0
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
