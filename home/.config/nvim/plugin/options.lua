840d415c77a831f90cb89b656a79826b34c13ab0
Larry Botha <lawrenceguy@gmail.com>
2025-11-23 14:50:02 +0200

    docs(options.lua): describe option accurately

--- home/.config/nvim/plugin/options.lua
+++ home/.config/nvim/plugin/options.lua
@@ -30,7 +30,7 @@ opt.wildmode = "list:longest,full" -- command <Tab> completion, list matches, th
 opt.winbar = "%=%m %f" -- add filename to winbar aligned to end
 opt.winminheight = 0 -- windows can be 0 line high
 opt.wrap = false -- don't wrap lines
-opt.winborder = "double" -- don't wrap lines
+opt.winborder = "double" -- use double line for floating window borders
 
 -- a: Suppress "hit-enter" prompt that appears when a command has completed
 -- t: Suppress "Terminal" message that appears when you enter a terminal buffer
