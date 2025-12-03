d973a1ee0aa1588de26696d155dda57d33925bcb
Larry Botha <lawrenceguy@gmail.com>
2025-12-04 00:19:59 +0200

    feat(nvim): add opencode

---
+++ home/.config/nvim/lua/custom/plugins/lazy/opencode.nvim.lua
@@ -0,0 +1,41 @@
+return {
+	"NickvanDyke/opencode.nvim",
+	dependencies = {
+		-- Recommended for `ask()` and `select()`.
+		-- Required for `snacks` provider.
+		---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+	},
+	config = function()
+		local opencode = require("opencode")
+
+		---@type opencode.Opts
+		vim.g.opencode_opts = {
+			-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+		}
+
+		-- Required for `opts.events.reload`.
+		vim.o.autoread = true
+
+		-- Recommended/example keymaps.
+		vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+			opencode.ask("@this: ", { submit = true })
+		end, { desc = "Ask opencode" })
+		vim.keymap.set({ "n", "x" }, "<leader>os", function()
+			opencode.select()
+		end, { desc = "Execute opencode action…" })
+		vim.keymap.set({ "n", "x" }, "<leader>op", function()
+			opencode.prompt("@this")
+		end, { desc = "Add to opencode" })
+		vim.keymap.set({ "n", "t" }, "<leader>ot",
+      opencode.toggle,
+      { desc = "Toggle opencode" },
+    )
+		vim.keymap.set("n", "<leader>ou", function()
+			opencode.command("session.half.page.up")
+		end, { desc = "opencode half page up" })
+		vim.keymap.set("n", "<leader>od", function()
+			opencode.command("session.half.page.down")
+		end, { desc = "opencode half page down" })
+	end,
+}
