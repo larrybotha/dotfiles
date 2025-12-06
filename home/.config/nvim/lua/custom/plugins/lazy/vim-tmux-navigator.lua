669c010b64f30bdfd8077604044d406bf26ce47a
Larry Botha <lawrenceguy@gmail.com>
2025-12-06 15:41:53 +0200

    fix(vim-tmux-navigator.lua): pass nav keymaps to lazygit to allow moving commits up/down

--- home/.config/nvim/lua/custom/plugins/lazy/vim-tmux-navigator.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/vim-tmux-navigator.lua
@@ -1,3 +1,21 @@
+-- Set up keymaps with process detection
+local function navigate_or_passthrough(key, nav_cmd)
+	return function()
+		-- lazygit: pass-through so that we can use <C-k> and <C-j> to move commits
+		local processes = { "lazygit" }
+		local bufname = vim.api.nvim_buf_get_name(0)
+
+		for _, proc_name in ipairs(processes) do
+			if bufname:match(proc_name) then
+				vim.api.nvim_feedkeys(vim.keycode("<C-" .. key .. ">"), "n", false)
+				break
+			else
+				vim.cmd(nav_cmd)
+			end
+		end
+	end
+end
+
 return {
 	{
 		"christoomey/vim-tmux-navigator",
@@ -9,12 +27,16 @@ return {
 			"TmuxNavigatePrevious",
 			"TmuxNavigatorProcessList",
 		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
+		init = function()
+			local function set_keymaps()
+				vim.keymap.set({ "n", "t" }, "<C-h>", navigate_or_passthrough("h", "TmuxNavigateLeft"))
+				vim.keymap.set({ "n", "t" }, "<C-j>", navigate_or_passthrough("j", "TmuxNavigateDown"))
+				vim.keymap.set({ "n", "t" }, "<C-k>", navigate_or_passthrough("k", "TmuxNavigateUp"))
+				vim.keymap.set({ "n", "t" }, "<C-l>", navigate_or_passthrough("l", "TmuxNavigateRight"))
+				vim.keymap.set({ "n", "t" }, "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>")
+			end
+
+			set_keymaps()
+		end,
 	},
 }
