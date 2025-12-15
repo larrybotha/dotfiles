95a3176199304b184d8af6ac7fc6d6af5373db12
Larry Botha <lawrenceguy@gmail.com>
2025-12-15 23:50:21 +0200

    feat(vectorcode): configure to lazily init lsp

--- home/.config/nvim/lua/custom/plugins/lazy/vectorcode.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/vectorcode.lua
@@ -6,6 +6,7 @@ return {
 	cond = function()
 		return vim.fn.executable("vectorcode") == 1
 	end,
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require("vectorcode").setup(
 			---@type VectorCode.Opts
@@ -13,6 +14,7 @@ return {
 				cli_cmds = {
 					vectorcode = "vectorcode",
 				},
+				-- options used when attaching to LSP, below
 				---@type VectorCode.RegisterOpts
 				async_opts = {
 					debounce = 100, -- Debounce time in milliseconds to avoid excessive updates
@@ -27,15 +29,30 @@ return {
 				},
 				async_backend = "lsp", -- Backend to use for async operations (default or lsp)
 				exclude_this = true, -- Exclude current buffer from operations
-				n_query = 1, -- Number of queries to send for each operation
+				n_query = 10, -- Number of queries to send for each operation
 				notify = true, -- Show notifications for operations
 				timeout_ms = 5000, -- Timeout in milliseconds for operations
 				on_setup = {
 					update = true, -- Update VectorCode on setup
-					lsp = true, -- LSP integration
+					lsp = false, -- Disable automatic LSP startup during setup
 				},
 				sync_log_env_var = false, -- Don't sync environment variables for logging
 			}
 		)
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function()
+				local cacher = require("vectorcode.cacher")
+				local bufnr = vim.api.nvim_get_current_buf()
+
+				cacher.utils.async_check("config", function()
+					cacher.lsp.register_buffer(bufnr, {
+						-- provide rich context for LSP queries
+						n_query = 10,
+					})
+				end, nil)
+			end,
+			desc = "Register buffer for VectorCode",
+		})
 	end,
 }
