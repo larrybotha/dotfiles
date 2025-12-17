f5104881964451d1f06967dd6ee4aa53cb941cab
Larry Botha <lawrenceguy@gmail.com>
2025-12-17 23:49:50 +0200

    fix(hydra.lua): navigate quickfix only as far as bounds of list

--- home/.config/nvim/lua/custom/plugins/lazy/hydra.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/hydra.lua
@@ -5,6 +5,25 @@ local function bind_diag_jump(n)
 end
 local next_diagnostic = bind_diag_jump(1)
 local prev_diagnostic = bind_diag_jump(-1)
+local next_qf_entry = function()
+	local qf_data = vim.fn.getqflist({
+		idx = 0,
+		nr = 0,
+		size = 0,
+	})
+
+	if qf_data.idx < qf_data.size then
+		vim.cmd.cnext()
+	end
+end
+local prev_qf_entry = function()
+	local qf_data = vim.fn.getqflist({ nr = 0, idx = 0 })
+
+	if qf_data.idx > 1 then
+		vim.cmd.cprevious()
+	end
+end
+
 return {
 	"nvimtools/hydra.nvim",
 	event = "VeryLazy",
@@ -37,19 +56,18 @@ return {
 			name = "Hydra: go to previous diagnostic",
 		})
 
-		-- TODO: determine how to silence errors if at ends of quickfix list
 		Hydra({
 			body = "]q",
-			config = { invoke_on_body = true, on_enter = vim.cmd.cnext },
-			heads = { { "q", vim.cmd.cnext, { silent = true } } },
+			config = { invoke_on_body = true, on_enter = next_qf_entry },
+			heads = { { "q", next_qf_entry, { silent = true } } },
 			mode = "n",
 			name = "next quickfix entry",
 		})
 
 		Hydra({
 			body = "[q",
-			config = { invoke_on_body = true, on_enter = vim.cmd.cprevious },
-			heads = { { "q", vim.cmd.cprevious, { silent = true } } },
+			config = { invoke_on_body = true, on_enter = prev_qf_entry },
+			heads = { { "q", prev_qf_entry, { silent = true } } },
 			mode = "n",
 			name = "previous quickfix entry",
 		})
@@ -69,5 +87,7 @@ return {
 			mode = "n",
 			name = "tabprevious",
 		})
+
+		vim.notify("hydra.nvim")
 	end,
 }
