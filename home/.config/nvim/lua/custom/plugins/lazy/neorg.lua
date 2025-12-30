5843420805d0a7103c54f170702fb5f784a5d723
Larry Botha <lawrenceguy@gmail.com>
2025-12-30 10:15:18 +0200

    feat: configure neorg workspaces

--- home/.config/nvim/lua/custom/plugins/lazy/neorg.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/neorg.lua
@@ -2,5 +2,22 @@ return {
 	"nvim-neorg/neorg",
 	lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 	version = "*", -- Pin Neorg to the latest stable release
-	config = true,
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.concealer"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							monitor_lizard = "~/.config/neorg/workspaces/monitor-lizard",
+							notes = "~/.config/neorg/workspaces/notes",
+							scratch = "~/.config/neorg/workspaces/scratch",
+							work = "~/.config/neorg/workspaces/work",
+						},
+					},
+				},
+			},
+		})
+	end,
 }
