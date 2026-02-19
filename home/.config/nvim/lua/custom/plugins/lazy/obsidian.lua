b1a2a2654bd5fb659e9295d320e529f43fb20d9d
Larry Botha <lawrenceguy@gmail.com>
2026-02-19 22:33:32 +0200

    feat(obsidian.lua): add keymap for opening 'everything' in nested neovim split

--- home/.config/nvim/lua/custom/plugins/lazy/obsidian.lua
+++ home/.config/nvim/lua/custom/plugins/lazy/obsidian.lua
@@ -6,6 +6,23 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("custom.plugins.obsidian")
+
+			-- Open 'everything' obsidian vault
+			--  - in terminal mode
+			--  - in vertical split
+			--  - in a nested Neovim instance
+			--  - restoring the most recent session
+			--
+			-- This allows for quickly getting back to the last location visited in
+			-- the obsidian vault, while allowing for navigation inside the project
+			-- independent of the Neovim instance from which it was opened
+			vim.keymap.set(
+				"",
+				"<leader>ve",
+				":vnew | terminal tmux new-session -A -s everything -c ~/projects/obsidian-vaults/everything/ nvim -c DoTheSession<CR>",
+				{ desc = "open 'everything' obsidian vault in vertical split terminal" }
+			)
+
 		end,
 	},
 }
