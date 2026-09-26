97251713bc861fdb7685357640e3f75757275bd7
Larry Botha <lawrenceguy@gmail.com>
2026-09-26 13:05:49 +0700

    feat(nvim): add videre for json visualisation

---
+++ home/.config/nvim/lua/custom/plugins/lazy/videre.nvim.lua
@@ -0,0 +1,11 @@
+return {
+	"Owen-Dechow/videre.nvim",
+	cmd = "Videre",
+	dependencies = {
+		"Owen-Dechow/graph_view_yaml_parser", -- Optional: add YAML support
+		"Owen-Dechow/graph_view_toml_parser", -- Optional: add TOML support
+	},
+	opts = {
+		box_style = "sharp",
+	},
+}
